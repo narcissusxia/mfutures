6d600d85ee5dca9202e64fe74e3b61cffd2ae1cf
narcissusxia <xialei1981>
2018-01-22 09:24:02 +0800

    commit

---
+++ x2.sh
@@ -0,0 +1,3 @@
+g++ -o test TraderSpi.cpp test2.cpp  ./lib_json/*.cpp -I./ -I /home/xialei/work/websocketpp/ -L/usr/lib/x86_64-linux-gnu/ -lboost_system -lboost_random -lboost_thread  -lboost_chrono -lrt -pthread -L/home/xialei/work/TraderAPI/ -lthosttraderapi -lPocoUtil -lPocoXML -lPocoNet -lPocoFoundation -I/include
+export LD_LIBRARY_PATH=/home/xialei/work/TraderAPI:$LD_LIBRARY_PATH
+./test
